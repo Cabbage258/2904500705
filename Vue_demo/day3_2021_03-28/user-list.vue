f11cff6ecccfb87fad51856c362460753717db22
CB_Cabbage <2904500705@qq.com>
2021-03-28 21:21:41 +0800

    笔记

---
+++ Vue_demo/day3_2021_03-28/user-list.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="list-unstyled">
+    <li v-for="user in users">
+      <slot :user="user">
+        {{user.id}}
+      </slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+name: "user-list"
+}
+</script>
+
+<style scoped>
+
+</style>
